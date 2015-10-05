0f38e4ab2f7532e5536fd1f2c6eaaf70b1f3caea
Jeong Hyun Kim <jhkim0508@gmail.com>
2015-10-05 14:58:35 -0500

    Update Code to get the average.R

--- Code to get the average.R
+++ Code to get the average.R
@@ -1,1 +1,2 @@
-
+average <- ddply(includeall, ~wustlid, summarise, average.dev.over.waves=mean(deviation, na.rm = T))
+abs.average <- ddply(includeall, ~wustlid, summarise, abs.average.dev.over.waves = mean(abs.deviation, na.rm=T))
